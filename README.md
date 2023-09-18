d56e28c03204e868d5b017d68e815011680874aa
Corbin Martin <corbin.martin@protonmail.com>
2023-09-18 00:58:27 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+this is a little version of grep I made going through the rust book.
+
+its pure functional so thats cool.
---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use std::{fs, env};
+
+fn get_arguments() -> (String , String) {
+    let args : Vec<String> = env::args().collect();
+    let error = "r_grep expected string and file path arguments";
+    (args.get(1).expect(error).clone(), args.get(2).expect(error).clone())
+}
+
+fn search_file() -> Vec<String> {
+    let (string, file_path) = get_arguments();
+    fs::read_to_string
+        (file_path)
+        .expect("could not read file")
+        .as_str()
+        .split("\n")
+        .enumerate()
+        .filter(|(_, line)| line.contains(&string))
+        .map(|(index, line)| format!("({}): {}", index + 1, line))
+        .collect()
+}
+
+fn main() {
+    search_file()
+        .iter()
+        .for_each(|line| println!("{}", line,))
+}
