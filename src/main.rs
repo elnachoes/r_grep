a9998a091fcdcfda78396458f0913a307ec58f8c
Corbin Martin <corbin.martin@protonmail.com>
2023-09-18 21:52:53 -0700

    refractored rgrep down to 12 lines

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,12 @@
-use std::{fs, env};
-
-fn get_arguments() -> (String , String) {
-    let args : Vec<String> = env::args().collect();
-    let error = "r_grep expected string and file path arguments";
-    (args.get(1).expect(error).clone(), args.get(2).expect(error).clone())
-}
-
-fn search_file() -> Vec<String> {
-    let (string, file_path) = get_arguments();
-    fs::read_to_string(file_path)
+fn main() {
+    let args : Vec<String> = std::env::args().collect();
+    let string = args.get(1).expect("r_grep expected string argument");
+    let file_path = args.get(2).expect("r_grep expected file argument");
+    std::fs::read_to_string(file_path)
         .expect("could not read file")
         .as_str()
         .split("\n")
         .enumerate()
-        .filter(|(_, line)| line.contains(&string))
-        .map(|(index, line)| format!("({}): {}", index + 1, line))
-        .collect()
-}
-
-fn main() {
-    search_file()
-        .iter()
-        .for_each(|line| println!("{}", line,))
+        .filter(|(_, line)| line.contains(string))
+        .for_each(|(index, line)| println!("({}): {}", index + 1, line))
 }
